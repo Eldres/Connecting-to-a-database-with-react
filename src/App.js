91f17d0dda12dd44f5e6623783053ea7bc5f7c11
Josh Nagel <nageljp27@gmail.com>
2022-07-12 15:28:42 -0600

    handled http errors with async/await func, and state management

--- src/App.js
+++ src/App.js
@@ -6,23 +6,35 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleFetchMovies = async () => {
     setIsLoading(true);
-    // GET request
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+    setError(null);
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
+    try {
+      // GET request
+      const response = await fetch("https://swapi.dev/api/films/");
 
-    setMovies(transformedMovies);
+      if (!response.ok) {
+        throw new Error(`Something went wrong. ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
 
     /* commented out below code in favor of using async/await
@@ -45,16 +57,22 @@ function App() {
     */
   };
 
+  let content = <p>Found no movies.</p>;
+
+  if (!isLoading && movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  } else if (!isLoading && error) {
+    content = <p>{error}</p>;
+  } else if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={handleFetchMovies}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
